c2b7bc75fa7a3cbcbb5905fe5da59a2d3fdfa3f3
Robin Hultman <robin.hultman@ibiz-solutions.se>
2015-02-13 03:46:01 -0800

    Initial checkin

---
+++ Src/HL7MessagePartCreator/HL7MessagePartCreator.Component.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using BizTalkComponents.Utils;
+
+namespace BizTalkComponents.HL7MessagePartCreator
+{
+    public partial class HL7MessagePartCreator
+    {
+        public string Description
+        {
+            get { return "Creates MSH and Z segments for HL7 messages."; }
+        }
+
+        public string Name
+        {
+            get { return "HL7MessagePartsCreator"; }
+        }
+
+        public string Version
+        {
+            get { return "1.0"; }
+        }
+
+        public void GetClassID(out Guid classID)
+        {
+            classID = Guid.Parse("DACFA6D5-EA1F-47A4-A74C-58C3C010F8EF");
+        }
+
+        public void InitNew()
+        {
+        }
+
+        public IEnumerator Validate(object projectSystem)
+        {
+            return ValidationHelper.Validate(this, false).ToArray().GetEnumerator();
+        }
+
+        public bool Validate(out string errorMessage)
+        {
+            var errors = ValidationHelper.Validate(this, true).ToArray();
+
+            if (errors.Any())
+            {
+                errorMessage = string.Join(",", errors);
+
+                return false;
+            }
+
+            errorMessage = string.Empty;
+
+            return true;
+        }
+
+        public IntPtr Icon { get { return IntPtr.Zero; } }
+    }
+}
---
+++ Src/HL7MessagePartCreator/HL7MessagePartCreator.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using BizTalkComponents.Utils;
+using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+using Microsoft.XLANGs.RuntimeTypes;
+using IComponent = Microsoft.BizTalk.Component.Interop.IComponent;
+
+namespace BizTalkComponents.HL7MessagePartCreator
+{
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [System.Runtime.InteropServices.Guid("23E5CD5D-B0D6-4546-8E86-05038ACE8542")]
+    [ComponentCategory(CategoryTypes.CATID_Any)]
+    public partial class HL7MessagePartCreator : IBaseComponent, IPersistPropertyBag, IComponent, IComponentUI
+    {
+        [Description("The document spec name of the MSH schema.")]
+        [DisplayName("DocumentSpecName")]
+        [RequiredRuntime]
+        public string DocumentSpecName { get; set; }
+
+        private const string DocumentSpecNamePropertyName = "DocumentSpecName";
+
+        public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+            string errorMessage;
+
+            if (!Validate(out errorMessage))
+            {
+                throw new ArgumentException(errorMessage);
+            }
+
+            var bodyPart = pContext.GetMessageFactory().CreateMessagePart();
+            bodyPart.Data = pInMsg.BodyPart.Data;
+            bodyPart.Charset = "utf-8";
+            bodyPart.ContentType = null;
+
+            var mshPart = CreateMSHSegment(pInMsg.Context, pContext);
+            mshPart.Charset = "utf-8";
+            mshPart.ContentType = null;
+
+            var zPart = CreateZSegment(pContext);
+            zPart.Charset = "utf-8";
+            pInMsg.RemovePart("Body");
+            pInMsg.AddPart("MSHSegment", mshPart, false);
+            pInMsg.AddPart("BodySegments", bodyPart, true);
+            pInMsg.AddPart("ZSegments", zPart, false);
+            pInMsg.Context = pInMsg.Context;
+
+            return pInMsg;
+        }
+
+        private IBaseMessagePart CreateMSHSegment(IBaseMessageContext ctx, IPipelineContext pCtx)
+        {
+            //Get a reference to the BizTalk schema.
+            var documentSpec = (DocumentSpec)pCtx.GetDocumentSpecByName(DocumentSpecName);
+
+            //Get a list of properties defined in the schema.
+            var annotations = documentSpec.GetPropertyAnnotationEnumerator();
+
+            var sw = new StringWriter(new StringBuilder());
+
+            //Create a new instance of the schema.
+            var doc = XDocument.Load(documentSpec.CreateXmlInstance(sw));
+            sw.Dispose();
+
+            //Write all properties to the message body.
+            while (annotations.MoveNext())
+            {
+                var annotation = (IPropertyAnnotation)annotations.Current;
+                var node = doc.XPathSelectElement(annotation.XPath);
+                object propertyValue;
+
+                if (ctx.TryRead(new ContextProperty(annotation.Name, annotation.Namespace), out propertyValue))
+                {
+                    node.Value = propertyValue.ToString();
+                }
+            }
+
+            //Make sure to remove any empty nodes.
+            doc.Descendants().Where(e => string.IsNullOrEmpty(e.Value)).Remove();
+
+            var ms = new MemoryStream();
+            var xws = new XmlWriterSettings { OmitXmlDeclaration = true, Indent = true };
+            using (var w = XmlWriter.Create(ms, xws))
+            {
+                doc.Save(w);
+            }
+
+            var mshPart = pCtx.GetMessageFactory().CreateMessagePart();
+
+            mshPart.Data = ms;
+            ms.Seek(0, SeekOrigin.Begin);
+            ms.Position = 0;
+
+            return mshPart;
+        }
+
+        private IBaseMessagePart CreateZSegment(IPipelineContext pCtx)
+        {
+            var part = pCtx.GetMessageFactory().CreateMessagePart();
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(string.Empty);
+            writer.Flush();
+            ms.Position = 0;
+
+            part.Data = ms;
+            return part;
+        }
+
+        public void Load(IPropertyBag propertyBag, int errorLog)
+        {
+            DocumentSpecName = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, DocumentSpecNamePropertyName), string.Empty);
+        }
+
+        public void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
+        {
+            PropertyBagHelper.WritePropertyBag(propertyBag, DocumentSpecNamePropertyName, DocumentSpecName);
+        }
+    }
+}
